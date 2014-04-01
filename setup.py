c5bca17a14cd57755d816090b9c2b3b9b743268a
Edd Barrett <vext01@gmail.com>
2014-04-01 16:55:28 +0100

    Add simple distutils build system. Tested on virtualenv.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2.7
+from distutils.core import setup
+from email_merger import PROG_VERSION_NUMERIC, PROG_NAME
+
+setup(name=PROG_NAME,
+    version=PROG_VERSION_NUMERIC,
+    author='Laurence Tratt',
+    author_email='laurie@tratt.net',
+    url='https://github.com/ltratt/email_merger',
+    scripts=['email_merger.py'],
+)
